1924b1907098c2a7066ac069dced266132c058af
Anjum Abbas <49648217+anjumabbas5@users.noreply.github.com>
2021-11-01 15:19:46 +0000

    API-5124 - Play 2.8 Upgrade (#17)
    Co-authored-by: Matthew Clark <mattclark-zerogravit@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,16 @@ import sbt._
 object AppDependencies {
 
   lazy val enumeratumVersion = "1.6.3"
-  lazy val bootstrapVersion = "5.3.0"
+  lazy val bootstrapVersion = "5.14.0"
   lazy val jacksonVersion = "2.12.2"
 
   val compile = Seq(
-    "uk.gov.hmrc"                     %% "bootstrap-backend-play-27" % bootstrapVersion,
+    "uk.gov.hmrc"                     %% "bootstrap-backend-play-28" % bootstrapVersion,
     "com.beachape"                    %% "enumeratum-play-json"       % enumeratumVersion,
     "com.fasterxml.jackson.core"       % "jackson-core"               % jacksonVersion,
     "com.fasterxml.jackson.core"       % "jackson-annotations"        % jacksonVersion,
     "com.fasterxml.jackson.core"       % "jackson-databind"           % jacksonVersion,
-    "com.fasterxml.jackson.module"     %% "jackson-module-scala"           % jacksonVersion,
+    "com.fasterxml.jackson.module"     %% "jackson-module-scala"      % jacksonVersion,
     "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml"    % jacksonVersion,
     "com.typesafe.play"               %% "play-json-joda"             % "2.9.0",
     "org.typelevel"                   %% "cats-core"                  % "2.4.2",
@@ -23,12 +23,10 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27"   % bootstrapVersion % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion % "test, it",
     "org.pegdown"             % "pegdown"                  % "1.6.0"  % "test, it",
     "org.jsoup"               % "jsoup"                    % "1.13.1" % Test,
-    "com.typesafe.play"      %% "play-test"                % current  % Test,
     "com.vladsch.flexmark"    % "flexmark-all"             % "0.36.8" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"  % "test, it",
     "org.mockito"            %% "mockito-scala-scalatest"  % "1.14.4" % "test, it",
     "com.github.tomakehurst"  % "wiremock"                 % "2.25.1" % "test, it",
     "com.github.tomakehurst"  % "wiremock-jre8-standalone" % "2.27.1" % "test, it"
